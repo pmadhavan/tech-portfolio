2f626fab2cd025772cb1c9c646af439c1aa1a450
pmadhavan <madhavan.padmaja@gmail.com>
2020-10-02 12:18:48 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Padmaja Madhavan', // Title for your website.
     tagline: 'Front End Engineer',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://pmadhavan.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'pmadhavan',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'https://www.linkedin.com/in/padmaja-madhavan-b918b899/',
         twitter: '',
-        github: '',
+        github: 'https://github.com/pmadhavan/',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
