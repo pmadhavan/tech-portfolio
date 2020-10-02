b84dde73b5ae06fc9d9d937de6f970b36581a506
pmadhavan <madhavan.padmaja@gmail.com>
2020-10-02 12:14:45 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,8 +12,8 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Padmaja Madhavan', // Title for your website.
+    tagline: 'Front End Engineer',
     profilePhoto: 'img/profile.png',
     url: 'https://joatmon08.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
